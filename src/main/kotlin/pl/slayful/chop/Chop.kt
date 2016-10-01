e84cdc90bbce3612e16a6656dc72e0706a2a69e1
Michał Turek <mturek@wikia-inc.com>
2016-10-01 16:12:11 +0200

    first koan: chop

---
+++ src/main/kotlin/pl/slayful/chop/Chop.kt
@@ -0,0 +1,8 @@
+package pl.slayful.chop
+
+class Chop {
+
+    fun chop(value: Int, array : IntArray): Int {
+         return -1
+    }
+}
---
+++ src/test/kotlin/pl/slayful/chop/ChopTest.kt
@@ -0,0 +1,33 @@
+package pl.slayful.chop
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+class ChopTest {
+
+    private var chop: Chop = Chop()
+
+    @Test fun testOk() {
+        assertEquals(-1, chop.chop(3, intArrayOf()))
+        assertEquals(-1, chop.chop(3, intArrayOf(1)))
+        assertEquals(0, chop.chop(1, intArrayOf(1)))
+        assertEquals(0, chop.chop(1, intArrayOf(1, 3, 5)))
+        assertEquals(1, chop.chop(3, intArrayOf(1, 3, 5)))
+        assertEquals(2, chop.chop(5, intArrayOf(1, 3, 5)))
+        assertEquals(-1, chop.chop(0, intArrayOf(1, 3, 5)))
+        assertEquals(-1, chop.chop(2, intArrayOf(1, 3, 5)))
+        assertEquals(-1, chop.chop(4, intArrayOf(1, 3, 5)))
+        assertEquals(-1, chop.chop(6, intArrayOf(1, 3, 5)))
+        assertEquals(0, chop.chop(1, intArrayOf(1, 3, 5, 7)))
+        assertEquals(1, chop.chop(3, intArrayOf(1, 3, 5, 7)))
+        assertEquals(2, chop.chop(5, intArrayOf(1, 3, 5, 7)))
+        assertEquals(3, chop.chop(7, intArrayOf(1, 3, 5, 7)))
+        assertEquals(-1, chop.chop(0, intArrayOf(1, 3, 5, 7)))
+        assertEquals(-1, chop.chop(2, intArrayOf(1, 3, 5, 7)))
+        assertEquals(-1, chop.chop(4, intArrayOf(1, 3, 5, 7)))
+        assertEquals(-1, chop.chop(6, intArrayOf(1, 3, 5, 7)))
+        assertEquals(-1, chop.chop(8, intArrayOf(1, 3, 5, 7)))
+    }
+
+}
